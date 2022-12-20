394a46ca4475348d35f7fe893e20be0a96c5f808
frontendprof <abdumaliksharipov@gmail.com>
2022-12-20 12:46:26 +0500

    feat: add scroll header

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,14 @@ var homeSwiper = new Swiper(".home-swiper", {
     clickable: true,
   },
 });
+
+// Scroll header
+
+function scrollHeader() {
+  const header = document.getElementById("header");
+
+  if (this.scrollY > 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+
+window.addEventListener("scroll", scrollHeader);

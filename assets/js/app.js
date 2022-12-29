9072d793067f945d180df96b99184c2b7c08038a
cyberweb8 <abdumalikshareef@gmail.com>
2022-12-29 21:38:02 +0500

    feat: add media queries

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,20 @@
+// show menu
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
+
 // show/hide cart
 const cart = document.getElementById('cart');
 const cartShop = document.getElementById('cart__shop');

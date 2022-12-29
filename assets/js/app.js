e7d323b5a5883622e51f93a4163bba76113ad4b3
cyberweb8 <abdumalikshareef@gmail.com>
2022-12-29 19:09:01 +0500

    feat: add show/hide login/cart/scrollup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,44 @@
+// show/hide cart
+const cart = document.getElementById('cart');
+const cartShop = document.getElementById('cart__shop');
+const cartClose = document.getElementById('cart-close');
+
+if (cartShop) {
+  cartShop.addEventListener('click', () => {
+    cart.classList.add('show-cart');
+  });
+}
+
+if (cartClose) {
+  cartClose.addEventListener('click', () => {
+    cart.classList.remove('show-cart');
+  });
+}
+
+// show/hide login
+const login = document.getElementById('login');
+const loginBtn = document.getElementById('login__button');
+const loginClose = document.getElementById('login-close');
+
+if (loginBtn) {
+  loginBtn.addEventListener('click', () => {
+    login.classList.add('show-login');
+  });
+}
+
+if (loginClose) {
+  loginClose.addEventListener('click', () => {
+    login.classList.remove('show-login');
+  });
+}
+
 // Home swiper
 
-var homeSwiper = new Swiper(".home-swiper", {
+var homeSwiper = new Swiper('.home-swiper', {
   spaceBetween: 30,
   loop: true,
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
   },
 });
@@ -12,19 +46,28 @@ var homeSwiper = new Swiper(".home-swiper", {
 // Scroll header
 
 function scrollHeader() {
-  const header = document.getElementById("header");
+  const header = document.getElementById('header');
 
-  if (this.scrollY > 50) header.classList.add("scroll-header");
-  else header.classList.remove("scroll-header");
+  if (this.scrollY > 50) header.classList.add('scroll-header');
+  else header.classList.remove('scroll-header');
 }
 
-window.addEventListener("scroll", scrollHeader);
+window.addEventListener('scroll', scrollHeader);
 
 // New swiper
 
-var newSwiper = new Swiper(".new-swiper", {
+var newSwiper = new Swiper('.new-swiper', {
   spaceBetween: 16,
   centeredSlides: true,
-  slidesPerView: "auto",
+  slidesPerView: 'auto',
   loop: true,
 });
+
+// scrollup
+function scrollUp() {
+  const scrollUpBtn = document.getElementById('scroll-up');
+  if (this.scrollY > 350) scrollUpBtn.classList.add('show__scroll');
+  else scrollUpBtn.classList.remove('show__scroll');
+}
+
+window.addEventListener('scroll', scrollUp);
